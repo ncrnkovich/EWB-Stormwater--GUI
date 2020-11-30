49279473f18b85c16ce6119d99ad7f0d639c6d4a
Gina Brusco <gbrusco@Ginas-MBP-9.home>
2020-11-30 13:39:41 -0500

    gigi!

--- quiz.js
+++ quiz.js
@@ -1,5 +1,7 @@
 function check() {
-
+	//Each variable represents an answer to a question
+	//Keep in order
+	//need parseInt() for free response questions
     var question1 = parseInt(document.quiz.question1.value, 10); //area of total property
     var question2 = parseInt(document.quiz.question2.value, 10); // area land
     var question3 = parseInt(document.quiz.question3.value, 10); // roof area
@@ -8,13 +10,9 @@ function check() {
 
     var numQuestions = 5; // number of questions
 
-    // OPTION CALCULATOR (points system)
-	var validOptions = ["after_submit1", "after_submit2", "after_submit3"];
-
-    var i;
-    for (i = 0; i < (numQuestions); i++) {
-        option[i] = 0; // initializes each option point counter as 0
-    }
+    // OPTION CALCULATOR
+	//for each option, add a new element (KEEP IN ORDER! & keep string name the same as the id)
+	var validOptions = ["after_submit1", "after_submit2", "after_submit3"]; 
 
     // Series of NESTED if/else statements... if (condition) then delete an option from the array (it is not applicable for the user's property type)
     if(question1 <=100){
@@ -25,16 +23,20 @@ function check() {
 
     // DISPLAY results
     //each element of this array coordinates with each option
-    var optionLinks = ["https://quizlet.com/", "https://nd.edu", "https://www.google.com/"];//more information about each option, just put in random links there for now
+	//more information about each option, just put in random links there for now
+	//keep links in order that correlates with validOptions order
+    var optionLinks = ["https://quizlet.com/", "https://nd.edu", "https://www.google.com/"];
 
 	if(isNaN(question1) || isNaN(question2) || isNaN(question3)){
       	document.getElementById("alert").style.visibility = "visible";
-       	document.getElementById("after_submit").style.visibility = "hidden";
+        document.getElementById("after_submit").style.visibility = "hidden";
 	}
 	
 	for(i=0; i< (validOptions.length); i++){
 		if(validOptions[i] != undefined){
+			document.getElementById("alert").style.visibility = "hidden";
 			document.getElementById(validOptions[i]).style.visibility = "visible";
+			document.getElementById("link" + i.toString(10)).href = optionLinks[i];
 		}
 	}
 
