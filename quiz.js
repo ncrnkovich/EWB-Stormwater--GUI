cd87b48ebe6368d0e1b9d95daa9df3fbbde3266e
ncrnkovich <62952982+ncrnkovich@users.noreply.github.com>
2020-11-30 20:45:10 -0600

    no changes made

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 function check() {
-	//Each variable represents an answer to a question
-	//Keep in order
-	//need parseInt() for free response questions
+    //Each variable represents an answer to a question
+    //Keep in order
+    //need parseInt() for free response questions
     var question1 = parseInt(document.quiz.question1.value, 10); //area of total property
     var question2 = parseInt(document.quiz.question2.value, 10); // area land
     var question3 = parseInt(document.quiz.question3.value, 10); // roof area
@@ -9,40 +9,39 @@ function check() {
     var question5 = document.quiz.question5.value;
 
     var numQuestions = 5; // number of questions
-	var i;
+    var i;
 
     // OPTION CALCULATOR
-	//for each option, add a new element (KEEP IN ORDER! & keep string name the same as the id)
-	var validOptions = ["after_submit1", "after_submit2", "after_submit3"]; 
+    //for each option, add a new element (KEEP IN ORDER! & keep string name the same as the id)
+    var validOptions = ["after_submit1", "after_submit2", "after_submit3"];
 
     // Series of NESTED if/else statements... if (condition) then delete an option from the array (it is not applicable for the user's property type)
-    if(question1 <=100){
-		delete validOptions[0]; //this means that the first option is not valid & deletes it from the array
-	}else{
-		delete validOptions[1];
-	}
+    if (question1 <= 100) {
+        delete validOptions[0]; //this means that the first option is not valid & deletes it from the array
+    } else {
+        delete validOptions[1];
+    }
 
     // DISPLAY results
     //each element of this array coordinates with each option
-	//more information about each option, just put in random links there for now
-	//keep links in order that correlates with validOptions order
-    var optionLinks = ["https://quizlet.com/", "https://nd.edu", "https://www.google.com/"];
+    //more information about each option, just put in random links there for now
+    //keep links in order that correlates with validOptions order
 
-	if(isNaN(question1) || isNaN(question2) || isNaN(question3)){
-      	document.getElementById("alert").style.visibility = "visible";
-        for(i=0; i<validOptions.length; i++){
-			document.getElementById("after_submit" + i.toString(10)).style.visibility = "hidden";
-		}
-	}
-	var str = null;
-	for(i=0; i< (validOptions.length); i++){
-		document.getElementById("alert").style.visibility = "hidden";	
-		if(validOptions[i] != undefined){
-			str = "link" + i.toString(10);
-			document.getElementById(validOptions[i]).style.visibility = "visible";
-			document.getElementById(str).href = optionLinks[i];
-		}
-	}
+    if (isNaN(question1) || isNaN(question2) || isNaN(question3)) {
+        document.getElementById("alert").style.visibility = "visible";
+        for (i = 0; i < validOptions.length; i++) {
+            document.getElementById("after_submit" + i.toString(10)).style.visibility = "hidden";
+        }
+    }
+    var str = null;
+    for (i = 0; i < (validOptions.length); i++) {
+        document.getElementById("alert").style.visibility = "hidden";
+        if (validOptions[i] != undefined) {
+            str = "link" + i.toString(10);
+            document.getElementById(validOptions[i]).style.visibility = "visible";
+            document.getElementById(str).href = optionLinks[i];
+        }
+    }
 
     /*if (isNaN(question1) || isNaN(question2) || isNaN(question3)) {
         document.getElementById("alert").style.visibility = "visible";
